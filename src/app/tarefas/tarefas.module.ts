5e458f859bd49b97437ab278fb0d8a775498639b
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2021-09-09 10:19:27 -0300

    rotas

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+
+
+
+
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    ListarTarefaComponent,
+    
+  ],
+  providers: [TarefaService]
+})
+export class TarefasModule { }
